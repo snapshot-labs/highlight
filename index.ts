8a82c389cb5f1da0782426e1bbbcd323fea60a6c
bonustrack <fabien@bonustrack.co>
2021-06-05 16:31:54 +0700

    Start with express

--- index.ts
+++ index.ts
@@ -1,40 +1,12 @@
-import Libp2p from 'libp2p';
-import WebSockets from 'libp2p-websockets';
-import WebRtcStar from 'libp2p-webrtc-star';
-import { NOISE } from 'libp2p-noise';
-import MPLEX from 'libp2p-mplex';
-import wrtc from 'wrtc';
+import express from 'express';
+import bodyParser from 'body-parser';
+import './node';
 
-(async () => {
-	const transportKey = WebRtcStar.prototype[Symbol.toStringTag];
+const app = express();
+const PORT = 3000;
 
-	const node = await Libp2p.create({
-		modules: {
-			transport: [WebSockets, WebRtcStar],
-			connEncryption: [NOISE],
-			streamMuxer: [MPLEX]
-		},
-		addresses: {
-			listen: [
-				'/ip4/127.0.0.1/tcp/8000/ws',
-				'/dns4/p2p-webrtc-star.herokuapp.com/tcp/443/wss/p2p-webrtc-star/'
-			]
-		},
-		config: {
-			transport: {
-				[transportKey]: {
-					wrtc
-				}
-			}
-		}
-	});
+app.use(bodyParser.urlencoded({ extended: false }));
 
-	// start libp2p
-	await node.start();
-
-	const advertiseAddrs = node.multiaddrs;
-	console.log('libp2p is advertising the following addresses: ', advertiseAddrs);
-
-	// stop libp2p
-	await node.stop();
-})();
+app.listen(PORT, () => {
+  console.log(`Listening at http://localhost:${PORT}`);
+});
