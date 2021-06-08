577284dde1006bcbbd0258a26b0afa4c7b6343c5
bonustrack <fabien@bonustrack.co>
2021-06-08 18:30:15 +0700

    Add GraphQL mutation

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -1,7 +1,7 @@
-import { ApolloServer } from 'apollo-server-express';
-import { logs, sendToPeers } from '../utils';
 import path from 'path';
 import fs from 'fs';
+import { ApolloServer } from 'apollo-server-express';
+import { logs, sendToPeers } from '../utils';
 import getInstance from '../network';
 
 export const schemaFile = path.join(__dirname, './schema.gql');
@@ -9,21 +9,20 @@ export const typeDefs = fs.readFileSync(schemaFile, 'utf8');
 
 export const resolvers = {
   Query: {
-    hello: async () => {
+    logs: () => {
+      return logs;
+    }
+  },
+  Mutation: {
+    send: async (parent, { msg }) => {
       const node = getInstance();
       if (node) {
-        const msg = {
+        await sendToPeers(node, node.peerStore.peers, {
           ts: parseInt((Date.now() / 1e3).toFixed()),
-          msg: '👋'
-        };
-
-        try {
-          await sendToPeers(node, node.peerStore.peers, msg);
-        } catch (e) {
-          console.error('send failed', e);
-        }
+          msg: msg || '👋'
+        });
       }
-      return logs;
+      return msg;
     }
   }
 };
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import dotenv from 'dotenv';
-dotenv.config();
-
 import express from 'express';
 import bodyParser from 'body-parser';
+
+dotenv.config();
 import server from './graphql';
 
 const app = express();
