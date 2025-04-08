7589267760a8e801fdf1f6565b9902669dd8582d
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2025-04-08 15:47:33 +0200

    chore: remove unused getStorage

--- src/api/utils.ts
+++ src/api/utils.ts
@@ -1,24 +1,3 @@
-import { BigNumber } from '@ethersproject/bignumber';
-import { concat, hexlify, hexZeroPad } from '@ethersproject/bytes';
-import { keccak256 } from '@ethersproject/keccak256';
-import { StaticJsonRpcProvider } from '@ethersproject/providers';
-
-export async function getStorage(contract, index, blockNum, chainId, address) {
-  const concated = concat([
-    hexZeroPad(address, 32),
-    hexZeroPad(BigNumber.from(index).toHexString(), 32)
-  ]);
-  const key = keccak256(hexlify(concated));
-  const provider = new StaticJsonRpcProvider(
-    `https://rpc.brovider.xyz/${chainId}`,
-    chainId
-  );
-
-  const result = await provider.getStorageAt(contract, key, blockNum);
-
-  return BigNumber.from(result).toBigInt().toString();
-}
-
 export async function getEntity(entity: any, id: string, indexerName: string) {
   let item = await entity.loadEntity(id, indexerName);
 
